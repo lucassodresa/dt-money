63885f335b01f5dbe0c003c0994ba47da86b3c39
lucassodresa <lucas.sodresa@gmail.com>
2022-07-01 22:00:15 +0100

    font configuration

--- src/sytles/global.ts
+++ src/sytles/global.ts
@@ -2,17 +2,17 @@ import { createGlobalStyle } from "styled-components";
 const styled = { createGlobalStyle };
 export const GlobalStyle = styled.createGlobalStyle`
   :root {
-    --background: #F0F2F5;
-    --red: #E62E4D
-    --blue: #5429CC
+    --background: #f0f2f5;
+    --red: #e62e4d;
+    --blue: #5429cc;
 
-    --blue-light: #6933ff
+    --blue-light: #6933ff;
 
-    --text-title: #363F5F
-    --text-body: #969CB3
+    --text-title: #363f5f;
+    --text-body: #969cb3;
 
-    --background:
-    --shape: #fff
+    --background: #f0f2f5;
+    --shape: #fff;
   }
 
   * {
@@ -36,9 +36,28 @@ export const GlobalStyle = styled.createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+  body,
+  input,
+  textarea,
+  button {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
+
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
