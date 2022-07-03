0d7708cfac9f604e9d9febf3824bf4bf834c02f4
lucassodresa <lucas.sodresa@gmail.com>
2022-07-03 14:17:30 -0700

    TransactionContext structure created

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./sytles/global";
+import { TransactionContext, TransactionsProvider } from "./TransactionContext";
 
 Modal.setAppElement("#root");
 
@@ -18,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewtransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -26,6 +27,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
+import { TransactionContext } from "../../TransactionContext";
 import { Container } from "./styles";
 
 export const Summary = () => {
+  const transactions = useContext(TransactionContext);
+
+  console.log(transactions);
+
   return (
     <Container>
       <div>
